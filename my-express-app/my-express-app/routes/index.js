b6e9e6231ab90ff22f7ce5ca2f8af9225950d659
Haqim Azhar <luqman.h@rocketbots.io>
2024-07-29 16:14:28 +0800

    test mart

--- my-express-app/my-express-app/routes/index.js
+++ my-express-app/my-express-app/routes/index.js
@@ -1,22 +1,8 @@
-// Base route file
 const express = require('express');
-const app = express();
-const port = 3000;
+const router = express.Router();
 
-// Import routes
-const baseRoutes = require('./routes/index');
-const subfolder1Routes = require('./subfolder1/routes/route1');
-const subfolder3Routes = require('./subfolder2/subfolder3/routes/route2');
-
-// Use routes
-app.use('/base', baseRoutes);
-app.use('/sub1', subfolder1Routes);
-app.use('/sub3', subfolder3Routes);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+router.get('/', (req, res) => {
+  res.send('Base Route tukar ni sikit');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+module.exports = router;
