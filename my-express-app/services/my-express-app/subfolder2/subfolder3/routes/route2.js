19291a03bfdcb790e89868d84bc1aadeb56c3dca
Haqim Azhar <luqman.h@rocketbots.io>
2024-07-31 17:51:20 +0800

    Refactor route-checker.yaml to improve router changes detection logic

--- my-express-app/services/my-express-app/subfolder2/subfolder3/routes/route2.js
+++ my-express-app/services/my-express-app/subfolder2/subfolder3/routes/route2.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Subfolder3 Route');
+  res.send('Subfolder3 Routs');
 });
 
 module.exports = router;
