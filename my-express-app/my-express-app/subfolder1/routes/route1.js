f44fb6eddb1a32aed598e15a340aeceb0c0c163b
Haqim Azhar <luqman.h@rocketbots.io>
2024-07-31 16:20:59 +0800

    hotfix: added new feature on route-checker.yaml

--- my-express-app/my-express-app/subfolder1/routes/route1.js
+++
@@ -1,9 +0,0 @@
-// Subfolder1 route file
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Subfolder1 Route');
-});
-
-module.exports = router;
---
+++ my-express-app/services/my-express-app/subfolder1/routes/route1.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const controllers = require('../app/controllers');
+const middlewares = require('../app/middlewares');
+
+const { ROUTE_NAMES } = CONSTANTS;
+
+const {
+    middleware: { cognitoAuthHandler }
+} = require('@respond-io/auth');
+
+const authOptions = {
+    cognito: {
+        tokenUse: 'id',
+        keysToVerify: ['user_id', 'role', 'email', 'session_id'],
+        debug: process.env.APP_ENV === 'local'
+    }
+};
+
+router.get(
+    '',
+    [
+        middlewares.http.service.initializeRouteName(ROUTE_NAMES.GET_INTEGRATIONS),
+        cognitoAuthHandler(authOptions.cognito),
+        middlewares.http.service.web,
+        middlewares.http.service.blockFrozenOrganization,
+        middlewares.http.service.acl.getSpaceUser,
+        middlewares.http.service.acl.checkRouteAccess
+    ],
+    (req, res, next) => new controllers.IntegrationController(req, res, next).getIntegrations()
+);
+
+router.delete(
+    '/:id',
+    [
+        middlewares.http.service.initializeRouteName(ROUTE_NAMES.DELETE_INTEGRATION),
+        cognitoAuthHandler(authOptions.cognito),
+        middlewares.http.service.web,
+        middlewares.http.service.blockFrozenOrganization,
+        middlewares.http.service.acl.getSpaceUser,
+        middlewares.http.service.acl.checkRouteAccess
+        // todo: check middlewares
+    ],
+    (req, res, next) => new controllers.IntegrationController(req, res, next).deleteIntegration()
+);
+
+module.exports = router;
